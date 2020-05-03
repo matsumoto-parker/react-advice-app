c93d5c4ca7b90393553ba628775ef1a25e349be8
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-05-03 11:01:44 +0900

    feat(App.js):get fetchData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div className="App"></div>;
+import './App.css';
+
+class App extends React.Component {
+  state = { advice: '' };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return <h1>APP</h1>;
+  }
 }
 
 export default App;
